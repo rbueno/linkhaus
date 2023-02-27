82a233109cb7352f4f19bf420dd8758513da4330
rbueno <rafaelbuenolink@gmail.com>
2023-02-27 15:54:18 -0300

    general update

--- src/sections/@dashboard/general/booking/BookingReservationStats.js
+++ src/sections/@dashboard/general/booking/BookingReservationStats.js
@@ -1,61 +1,69 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 // @mui
-import { Card, Box, Typography } from '@mui/material';
+import { Card, CardHeader, Box } from '@mui/material';
 // components
+import { CustomSmallSelect } from '../../../../components/custom-input';
 import Chart, { useChart } from '../../../../components/chart';
 
 // ----------------------------------------------------------------------
 
-export default function BookingReservationStats() {
+BookingReservationStats.propTypes = {
+  chart: PropTypes.object,
+  title: PropTypes.string,
+  subheader: PropTypes.string,
+};
 
+export default function BookingReservationStats({ title, subheader, chart, ...other }) {
+  const { categories, colors, series, options } = chart;
 
-  
+  const [seriesData, setSeriesData] = useState('Cliques');
 
-
-  const demostate = {
-          
-    series: [{
-        name: "Desktops",
-        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-    }],
-    options: {
-      chart: {
-        height: 350,
-        type: 'line',
-        zoom: {
-          enabled: false
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        curve: 'straight'
-      },
-      title: {
-        text: 'Product Trends by Month',
-        align: 'left'
-      },
-      grid: {
-        row: {
-          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-          opacity: 0.5
-        },
-      },
-      xaxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-      }
+  const chartOptions = useChart({
+    colors,
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ['transparent'],
     },
-  
+    xaxis: {
+      categories,
+    },
+    tooltip: {
+      y: {
+        formatter: (value) => `$${value}`,
+      },
+    },
+    ...options,
+  });
 
-}
   return (
-    <Card >
-      <Chart options={demostate.options} series={demostate.series} type="line" height="350px" />
-      {/* <Box>
-        <Typography>Teste</Typography>
-      </Box> */}
+    <Card {...other}>
+      {/* <CardHeader
+        title={title}
+        subheader={subheader}
+        action={
+          <CustomSmallSelect
+            value={seriesData}
+            onChange={(event) => setSeriesData(event.target.value)}
+          >
+            {series.map((option) => (
+              <option key={option.type} value={option.type}>
+                {option.type}
+              </option>
+            ))}
+          </CustomSmallSelect>
+        }
+      /> */}
+
+      {/* {series.map((item) => (
+        <Box key={item.type} sx={{ mt: 3, mx: 3 }} dir="ltr">
+          {item.type === seriesData && (
+            <Chart type="bar" series={item.data} options={chartOptions} height={364} />
+          )}
+        </Box>
+      ))} */}
+      <Chart type="bar" series={series[0].data} options={chartOptions} height={364} />
     </Card>
   );
 }
