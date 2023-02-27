90ee9cab1f05914c4c3c8ee59594e730b5b69307
rbueno <rafaelbuenolink@gmail.com>
2023-02-27 13:14:19 -0300

    general update

--- src/sections/@dashboard/general/booking/BookingReservationStats.js
+++ src/sections/@dashboard/general/booking/BookingReservationStats.js
@@ -34,12 +34,50 @@ export default function BookingReservationStats({ title, subheader, chart, ...ot
     ...options,
   });
 
+
+  const demostate = {
+          
+    series: [{
+        name: "Desktops",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+    }],
+    options: {
+      chart: {
+        height: 350,
+        type: 'line',
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      title: {
+        text: 'Product Trends by Month',
+        align: 'left'
+      },
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5
+        },
+      },
+      xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+      }
+    },
+  
+
+}
   return (
     <Card {...other}>
-      {/* <Chart type="bar" series={series[0].data} options={chartOptions} height={364} /> */}
-      <Box>
+      <Chart options={demostate.options} series={demostate.series} type="line" height={350} />
+      {/* <Box>
         <Typography>Teste</Typography>
-      </Box>
+      </Box> */}
     </Card>
   );
 }
