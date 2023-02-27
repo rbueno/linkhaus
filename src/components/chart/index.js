c3082be4499c5e36629c1fda63ea67b74dec6881
rbueno <rafaelbuenolink@gmail.com>
2023-02-27 12:11:22 -0300

    general update

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
-
-const Chart = dynamic(() => import('react-apexcharts'), { ssr: true });
+import Chart from 'react-apexcharts'
+// const Chart = dynamic(() => import('react-apexcharts'), { ssr: true });
 
 // ----------------------------------------------------------------------
 
